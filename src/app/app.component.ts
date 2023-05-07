150592191605882cf98feae8c3ff286530f5e10c
ufuk kuru <uege60@gmail.com>
2023-05-07 17:35:28 +0300

    Angular NgForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngForm';
+
+  constructor(
+    private _date:DatePipe
+  ){
+    const transformedDate = this._date.transform(new Date(), 'yyyy-MM-dd');
+    this.addModel.dateOfStart = transformedDate !== null ? transformedDate : '';
+  }
+
+  updateModel = new Employee()
+  addModel = new Employee()
+  employees:Employee[]=[]
+  index:number= 0
+  isUpdateFormActive:boolean=false
+
+  save(form:NgForm){
+    if(form.valid){
+      this.employees.push(this.addModel)
+      this.addModel = new Employee()
+      const transformedDate = this._date.transform(new Date(), 'yyyy-MM-dd');
+    this.addModel.dateOfStart = transformedDate !== null ? transformedDate : '';
+    }
+  }
+
+  get(model:Employee, index:number){
+    this.index = index
+    this.updateModel= {...model}
+    this.isUpdateFormActive = true
+  }
+
+  cancel(){
+    this.isUpdateFormActive=false
+  }
+
+  update(form:NgForm){
+    if(form.valid){
+      this.employees[this.index]= this.updateModel
+      this.isUpdateFormActive=false
+    }
+  }
+}
+
+class Employee{
+  name:string=""
+  profession:string=""
+  dateOfStart: string= ""
 }
